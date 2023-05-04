660ac277e2083fb49905702492479f1ca0d1d1a0
Anesazhegrova <120370272+Anesazhegrova@users.noreply.github.com>
2023-05-04 22:07:56 +0200

    modeli brand

---
+++ shoeshop/shoeshop/Model/Brand.cs
@@ -0,0 +1,12 @@
+﻿namespace shoeshop.Model
+{
+    public class Brand
+    {
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+        public string BrandDescription { get; set; }
+        public string ImageOfLogo { get; set; }
+        public string Website { get; set; }
+
+    }
+}
