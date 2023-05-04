a731000c1061d0c297cf43b5dd5698583265fe5a
Anesazhegrova <120370272+Anesazhegrova@users.noreply.github.com>
2023-05-04 21:00:07 +0200

    Update Category.cs

--- shoeshop/shoeshop/Model/Category.cs
+++ shoeshop/shoeshop/Model/Category.cs
@@ -2,7 +2,7 @@
 {
     public class Category
     {
-        public int CategoryId { get; set; }
+        public int CategroyId { get; set; }
         public string CategoryName { get; set; }
         public string CategoryDescription { get; set; }
         public string ImageOfCategory { get; set; }
