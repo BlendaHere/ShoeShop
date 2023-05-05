5c1feabf887722357031dbfd848c5159fd3c1385
BlendaHere <110112052+BlendaHere@users.noreply.github.com>
2023-05-05 21:35:08 +0200

    Modeli i Blogut

---
+++ shoeshop/shoeshop/Model/Blog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace shoeshop.Model
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime DatePosted { get; set; }
+        public string Content { get; set; }
+        public string ImageName { get; set; }
+
+    }
+}
+
