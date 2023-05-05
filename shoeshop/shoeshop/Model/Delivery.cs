c034e25eea3eb8f308af07789f42ce4005f5b4b6
BlendaHere <110112052+BlendaHere@users.noreply.github.com>
2023-05-05 21:32:20 +0200

    Modeli i Delivery

---
+++ shoeshop/shoeshop/Model/Delivery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace shoeshop.Model
+{
+    public class Delivery
+    {
+        public int DeliveryId { get; set; }
+        public int OrderId { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public string DeliveryAddress { get; set; }
+        public string DeliveryStatus { get; set; }
+
+
+    }
+}
+
