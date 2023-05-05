b6bed4199fdbedd36b4374dbcccd3703db9e1dd0
Anesazhegrova <120370272+Anesazhegrova@users.noreply.github.com>
2023-05-05 02:47:46 +0200

    Modeli i ClientOrder

---
+++ shoeshop/shoeshop/Model/ClientOrder.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace shoeshop.Model
+{
+    public class ClientOrder
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int ClientId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public string Size { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string PaymentMethod { get; set; }
+    }
+}
