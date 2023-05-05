25bc10a2cee8f946673b478d1faa3e1cb6f1d038
BlendaHere <110112052+BlendaHere@users.noreply.github.com>
2023-05-05 21:38:47 +0200

    Modeli i Staff-it

---
+++ shoeshop/shoeshop/Model/Staff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace shoeshop.Model
+{
+    public class STAFF
+    {
+        public int StaffId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime HireDate { get; set; }
+        public string Position { get; set; }
+        public string EmploymentStatus { get; set; }
+        public string ImageName { get; set; }
+
+
+    }
+}
+
