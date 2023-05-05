dddd2a2c614449a0a01b0227ebde2cdf78b69c27
BlendaHere <110112052+BlendaHere@users.noreply.github.com>
2023-05-05 04:30:07 +0200

    Modeli i WishList-es

---
+++ shoeshop/shoeshop/Model/WishList.cs
@@ -0,0 +1,14 @@
+﻿
+      namespace shoeshop.Model
+    {
+        public class WishList
+        {
+            public int WishListId { get; set; }
+            public string ProductName { get; set; }
+            public string ProductDescription { get; set; }
+            public decimal Price { get; set; }
+
+
+        }
+    }
+
