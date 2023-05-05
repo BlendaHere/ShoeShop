dfdba0cfdfcf4d6f6a6d67bd4f60609a456fdc4f
BlendaHere <110112052+BlendaHere@users.noreply.github.com>
2023-05-05 21:37:12 +0200

    Modeli i Inspiration

---
+++ shoeshop/shoeshop/Model/Inspiration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace shoeshop.Model
+{
+    public class Inspiration
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public DateTime DatePosted { get; set; }
+        public string Description { get; set; }
+        public string ImageName { get; set; }
+
+    }
+}
