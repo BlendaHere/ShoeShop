325782f6725ac6b9b604fdd5e4607242c94cf29a
BlendaHere <110112052+BlendaHere@users.noreply.github.com>
2023-05-05 04:34:12 +0200

    Kontrolleri i WishList-es.

---
+++ shoeshop/shoeshop/Controllers/WishListController.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using shoeshop.Model;
+using System.IO;
+
+
+namespace shoeshop.Controllers
+
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WishListController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+        public WishListController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                            select WishListId,ProductName,
+                                   ProductDescription,Price
+                              from WishList ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ShoeshopAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(WishList wl)
+        {
+            string query = @"
+                             insert into WishList
+                             values (@ProductName, @ProductDescription,@Price)";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ShoeshopAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@ProductName", wl.ProductName);
+                    myCommand.Parameters.AddWithValue("@ProductDescription", wl.ProductDescription);
+                    myCommand.Parameters.AddWithValue("@Price", wl.Price);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(WishList wl)
+        {
+            string query = @"
+                           update WishList
+                           set ProductName= @ProductName,
+                            ProductDescription=@ProductDescription,
+                            Price=@Price
+                            where WishListId=@WishListId
+                            ";
+
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ShoeshopAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@WishListId", wl.WishListId); ;
+                    myCommand.Parameters.AddWithValue("@ProductName", wl.ProductName);
+                    myCommand.Parameters.AddWithValue("@ProductDescription", wl.ProductDescription);
+                    myCommand.Parameters.AddWithValue("@Price", wl.Price);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                           delete from WishList
+                            where WishListId=@WishListId
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ShoeshopAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@WishListId", id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+
+
+
+
+    }
+}
